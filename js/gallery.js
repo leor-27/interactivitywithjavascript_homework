64b8253a40fd7535404282e98e1963196ece40b4
Leo Angelo Larre Agpoon <leoangelolarreagpoon@gmail.com>
2025-07-13 10:24:58 +0800

    Updates

--- js/gallery.js
+++ js/gallery.js
@@ -1,6 +1,6 @@
 function upDate(previewPic) {
     console.log(previewPic);
-    document.getElementById("image").style.backgroundImage = url(previewPic.src);
+    document.getElementById("image").style.backgroundImage = url('previewPic.src');
     document.getElementById("image").innerHTML = previewPic.alt;
 /* In this function you should 
     1) change the url for the background image of the div with the id = "image" 
@@ -14,7 +14,6 @@ function upDate(previewPic) {
 }
 
 function unDo() {
-    document.getElementById("image").style.backgroundImage = url('');
 
 /* In this function you should 
     1) Update the url for the background image of the div with the id = "image" 
