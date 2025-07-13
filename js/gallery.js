2257a99ebb8678319d28c91d1c032e2fa1bdde34
Leo Angelo Larre Agpoon <leoangelolarreagpoon@gmail.com>
2025-07-13 09:55:22 +0800

    Update upDate function to set background image from previewPic source

--- js/gallery.js
+++ js/gallery.js
@@ -1,7 +1,10 @@
 function upDate(previewPic) {
+    document.getElementById("image").style.backgroundImage = previewPic.src;
 /* In this function you should 
     1) change the url for the background image of the div with the id = "image" 
     to the source file of the preview image
+
+
     
     2) Change the text  of the div with the id = "image" 
     to the alt text of the preview image 
